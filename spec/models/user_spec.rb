20e82c5fbabf3083daf5b251cf57cc3e86750ab1
Vasyl <hupkav@gmail.com>
2015-09-25 12:45:06 +0300

    Add user following5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -126,7 +126,6 @@ describe "remember token" do
   end
 
 describe "micropost associations" do
-
     before { @user.save }
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -160,10 +159,21 @@ describe "micropost associations" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
